5d3bcb816f3e7f86c570de7df83863632b645669
VitaliiMalatok <palinamalatokpolish@gmail.com>
2024-11-01 17:04:50 +0100

    fixed type example

--- task_2/variable_var_let.ts
+++ task_2/variable_var_let.ts
@@ -212,3 +212,15 @@ let age1;
 if (age1 === undefined) {
     console.log("The age is undefined.");
 }
+
+//
+type Person = {name: string; age: number};
+// расширяем псевдоним Person
+type Employee = Person & {company: string};
+let tom: Person = {name: "Tom", age: 36};
+let bob: Employee = {name: "Bob", age: 41, company: "Microsoft"};
+function printPerson(user: Person){
+    console.log(`Name: ${user.name}  Age: ${user.age}`);
+}
+printPerson(tom);
+printPerson(bob);  // bob представляет Employee, но он также соответствует псевдониму Person
