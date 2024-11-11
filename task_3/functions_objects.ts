f831b0be2090501759c5fd59b0e23e6ca9389ba6
VitaliiMalatok <palinamalatokpolish@gmail.com>
2024-11-11 12:34:55 +0100

    add main information about object

--- task_3/functions_objects.ts
+++ task_3/functions_objects.ts
@@ -3,7 +3,7 @@
 function sum(a: number, b: number): number {
     return a + b;
 }
-let resultSum = sum(10, 11); // 21
+const resultSum = sum(10, 11); // 21
 console.log(resultSum);
 
 // if the function does not return anything, then specify the void type
@@ -21,16 +21,65 @@ function idUserName(firstName: string, secondName?:string): string {
     else
         return firstName;
 }
-let name1 = idUserName("Vitali", "Malatok"); // Vitali--Malatok
+const name1 = idUserName("Vitali", "Malatok"); // Vitali--Malatok
 console.log(name1);
-let name2 = idUserName("Palina"); // Palina
+const name2 = idUserName("Palina"); // Palina
 console.log(name2);
 
 // default value in function
 function getUserName(firstName: string, secondName: string = "MALATOK"): string {
     return firstName + "--" + secondName
 }
-let name3 = getUserName("Vitali", "Malatok"); // Vitali--Malatok
+const name3 = getUserName("Vitali", "Malatok"); // Vitali--Malatok
 console.log(name3);
-let name4 = getUserName("Palina"); // Palina--MALATOK
+const name4 = getUserName("Palina"); // Palina--MALATOK
 console.log(name4);
+
+// function expression or anonymous functions
+const massage = function(): void {
+    console.log("halo, halo");
+}
+massage(); // halo, halo
+
+// local function
+// a local function is a function inside another function
+// a local function can only be called within a surrounding function
+function printText(): void {
+    printHello();
+    function printHello(): void {
+        console.log("Hello");
+    }
+}
+printText(); // Hello
+
+// arrow function
+const hello = ()=> console.log("hello");
+hello(); // hello
+
+const printMes = mes=> console.log(mes);
+printMes("hello"); // hello
+const addSum = (x, y)=> console.log(x + y);
+addSum(1, 2); // 3
+const sumNum = (x, y)=> x + y;
+console.log(sumNum(2, 3)); // 5
+
+// object
+const person = {name: "Vitali", age: 36};
+for (let element in person) {
+    console.log(person[element]); // Vitali, 36
+}
+
+// optional object property
+let person1: {name: string, age?: number};
+person1 = { name: "Tom", age: 23 };
+console.log(person1.name);   // Tom
+person1 = { name: "Bob"};    
+console.log(person1.name);   // Bob
+
+// object in function
+function printUser(user: { name: string; age: number}) {
+    console.log(`name: ${user.name}  age: ${user.age}`);
+  }
+  let bob = {name: "Bob", age: 44, isMarried: true};
+  printUser(bob);  // name: Bob  age: 44
+
