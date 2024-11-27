e031e4a1234cd2f2a8d2bb8007d1881b22873a44
VitaliiMalatok <palinamalatokpolish@gmail.com>
2024-11-27 15:20:42 +0100

    fixed rest and spread operators

--- task_3/additional_information_functions_objects.ts
+++ task_3/additional_information_functions_objects.ts
@@ -13,3 +13,19 @@ const sayHej = function(): void {
     console.log("hej");
 };
 sayHej(); // hej
+
+// spread operator
+// allows you to decompose an array into individual values
+const userName = ["Vitali", "Palina", "Alina"];
+console.log(...userName); // Vitali Palina Alina
+
+const maxValueNumber = [5, 4, 6, 2, 7, 8, 9, 3];
+console.log(Math.max(...maxValueNumber)); // 9
+
+// rest operator
+// the rest operator puts the rest of some specific user-supplied values into a array
+let userId = function(fistName: string, secondName: string, ...otherParam): string {
+    return otherParam;
+}
+console.log(userId("Vitali", "Malatok", "date of bith", "last name")); // [ 'date of bith', 'last name' ]
+
